59e57efdf83106388a51bd1b6fff931104135dd2
Anish <anishb1010@gmail.com>
2020-11-26 02:34:47 -0500

    navigator node accepts heading, vel cmd then publishes vel. vel publishing TODO

---
+++ flightros/include/flightros/navigator.hpp
@@ -0,0 +1,50 @@
+#ifndef NAVIGATOR_H_
+#define NAVIGATOR_H_
+
+#include <ros/ros.h>
+#include <std_msgs/Float32.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <nav_msgs/Odometry.h>
+
+class PropNavMission{
+
+    private:
+
+        // ROS stuff
+        ros::NodeHandle _nh;
+        ros::Rate _rate;
+
+        // subscribers
+        ros::Subscriber _cmd_heading_sub;
+        ros::Subscriber _cmd_velocity_sub;
+        ros::Subscriber _odom_sub;
+
+        // publishers
+        ros::Publisher _cmd_pub;
+
+        // services
+
+        // initialization functions
+        void initializeSubscribers();
+        void initializePublishers();
+        void initializeServices();
+
+        // callback functions
+        void _cmd_heading_sub(const std_msgs::Float32::ConstPtr& msg);
+        void _cmd_velocity_sub(const std_msgs::Float32::ConstPtr& msg);
+        double _cmd_heading, _cmd_velocity;
+        void odom_cb(const nav_msgs::Odometry::ConstPtr& msg);
+        nav_msgs::Odometry _odom;
+
+        // timing variables
+        ros::Time _last_cmd;
+
+    public:
+
+        PropNavMission(ros::NodeHandle* nh, double freq);
+
+        void run();
+
+}
+
+#endif
